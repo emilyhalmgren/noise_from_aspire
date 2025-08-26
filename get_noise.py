e0f08f1858ee2714d48bc06ed3d9e3dcce7b8349
Emily Almgren <ea377@cornell.edu>
2025-08-26 16:10:24 -0400

    show

--- get_noise.py
+++ get_noise.py
@@ -30,21 +30,18 @@ plt.hist(df["alignments2D/cross_cor"], bins=50)
 plt.xlabel("2D alignment cross-correlation")
 plt.ylabel("Number of particles")
 #plt.title("Distribution of per-particle alignment scores")
-plt.show()
 
 plt.subplot(2, 2)
 plt.hist(df["alignments2D/class_posterior"], bins=50)
 plt.xlabel("Probability of assignment to the chosen class")
 plt.ylabel("Number of particles")
 #plt.title("Distribution of per-particle alignment scores")
-plt.show()
 
 plt.subplot(2, 1)
 plt.hist(df["alignments2D/weight"], bins=50)
 plt.xlabel("Weight given by cryoSPARC")
 plt.ylabel("Number of particles")
 #plt.title("Distribution of per-particle alignment scores")
-plt.show()
 
 # Model-vs-residual SNR
 snr_slice = df["alignments2D/slice_pow"] / df["alignments2D/resid_pow"]
